08df05fc4ee307cf9ea32fadeb62592c78e6d5ab
Huggins, Wyatt <wyatt.huggins@okstate.edu>
2026-02-03 12:47:29 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace WH_Basic_App_GitHub
+{
+    public class Class1
+    {
+        public string Name { get; }
+        public Class1()
+        {
+            Name = "[Wyatt Huggins] - The Code Leader";
+        }
+    }
+}
